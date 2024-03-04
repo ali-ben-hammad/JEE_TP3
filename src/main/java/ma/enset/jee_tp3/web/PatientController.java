20d2f656a50ba9f4af92d1be197d99504fadedcc
ali-ben-hammad <ali.benhammad-etu@etu.univh2c.ma>
2024-03-04 19:05:24 +0100

    added bootstrap display patients in table

--- src/main/java/ma/enset/jee_tp3/web/PatientController.java
+++ src/main/java/ma/enset/jee_tp3/web/PatientController.java
@@ -1,8 +1,12 @@
 package ma.enset.jee_tp3.web;
 
+import ma.enset.jee_tp3.entities.Patient;
 import ma.enset.jee_tp3.repository.PatientRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class PatientController {
@@ -14,7 +18,9 @@ public class PatientController {
     }
 
     @GetMapping ("/index")
-    public String index() {
+    public String index(Model model) {
+        List<Patient> patients  = patientRepository.findAll();
+        model.addAttribute("patients", patients);
         return "patients";
     }
 }
